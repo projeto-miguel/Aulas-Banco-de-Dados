b690570f25c018dbe116bc66b1d0efe45a156072
Miguel Carneiro de Mendonça <100208432+projeto-miguel@users.noreply.github.com>
2022-03-04 08:47:16 -0300

    Add files via upload

---
+++ aula_02.sql
@@ -0,0 +1,25 @@
+
+create database cadastro;
+use cadastro;
+create table pessoas(
+	nome varchar(30),
+    idade int,
+    altura decimal(3,2),
+    sexo char(1),
+    peso decimal(5,2),
+    nacionalidade varchar(30)
+
+);
+
+insert into pessoas
+(nome,idade,altura,sexo,peso,nacionalidade)
+values
+("Godofredo",45,1.90,"M",80.00,"Brasileiro");
+
+select * from pessoas;
+
+insert into pessoas
+(nome,idade,altura,sexo,peso,nacionalidade)
+values
+("Dolores",28,1.50,"F",33.35,"Mexicana"),
+("Godolores",14,1.21,"F",25.00,"Americana");
