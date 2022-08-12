024b8b67739ed1481c6c2eb577cb8e0b7badf91d
Miguel Carneiro de Mendonça <100208432+projeto-miguel@users.noreply.github.com>
2022-08-12 07:55:51 -0300

    Add files via upload

---
+++ Bases de dados/exercicios/clientes/compra.sql
@@ -0,0 +1,26 @@
+
+CREATE DATABASE mercado;
+USE mercado;
+
+CREATE TABLE produto (
+codigo_produto int PRIMARY KEY,
+nome_produto varchar(255),
+cor varchar(255),
+preco decimal(8,2)
+);
+
+CREATE TABLE cliente (
+id_cliente int PRIMARY KEY,
+email varchar(255),
+estado varchar(255),
+nome_cliente varchar(255)
+);
+
+CREATE TABLE compra (
+nf int PRIMARY KEY AUTO_INCREMENT,
+data_compra datetime,
+id_cliente int,
+codigo_produto int,
+FOREIGN KEY(id_cliente) REFERENCES cliente (id_cliente),
+FOREIGN KEY(codigo_produto) REFERENCES produto (codigo_produto)
+);
